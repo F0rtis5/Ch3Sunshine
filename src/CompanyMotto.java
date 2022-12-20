befc72f9c96f2691b101dbec5b3f241cb42ce06d
F0rtis5 <117367589+F0rtis5@users.noreply.github.com>
2022-12-20 08:49:28 -0600

    init

---
+++ src/CompanyMotto.java
@@ -0,0 +1,15 @@
+/**
+ * Display class to be called from Ch3Sunshine to display company motto
+ */
+
+public class CompanyMotto {
+    public static void main(String[] args) {getMotto();}
+
+        public static void getMotto() {
+            System.out.println("**************************");
+            System.out.println("Sunshine Seashore Supplies");
+            System.out.println("******* We Sea You *******");
+            System.out.println("**************************");
+        }
+    }
+
